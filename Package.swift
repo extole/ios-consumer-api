132be83638d7c2dac8c9f1146af6bb25488e69a1
alexanderkranga <acranga@extole.com>
2021-10-19 18:42:14 +0300

    use swift tools 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ExtoleConsumerAPI",
     platforms: [
-        .iOS(.v12),
-        .macOS(.v10_15)
+        .iOS(.v12)
     ],
     products: [
         .library(
