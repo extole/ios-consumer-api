85f43f52cf79518fb8e2d5b482ba2decc6708d22
diftodi <diftodi@extole.com>
2025-04-22 16:57:31 +0300

    iOS min version 13

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ExtoleConsumerAPI",
     platforms: [
-        .iOS(.v10),
-        .macOS(.v10_15)
+        .iOS(.v13),
     ],
     products: [
         .library(
